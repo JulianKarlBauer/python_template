5096703753f647f4362365f0e9d84d60d8260d81
Julian Karl Bauer <juliankarlbauer@gmx.de>
2025-10-26 09:19:30 +0100

    Consume package data

--- scripts/s001.py
+++ scripts/s001.py
@@ -1,3 +1,10 @@
 import example
+import importlib
+import yaml
 
-print("hi")
+print(f"{example.__version__=}")
+
+with importlib.resources.files("example.data").joinpath("01.yml").open("r") as myfile:
+    content = yaml.safe_load(myfile)
+
+print(f"{content=}")
